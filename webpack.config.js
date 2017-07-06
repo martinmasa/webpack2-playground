4306128c13ec73692d74957ccc9f6d0e873da3b9
Martin M <mxa.code@gmail.com>
2017-07-07 00:13:31 +0100

    Configure style, css and sass loaders with Extract Text Webpack Plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: `${__dirname}/dist`,
     filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.scss$/, 
+        use: ExtractTextWebpackPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader',
+          publicPath: '/dist'
+        })
+      }
+    ]
   },
   plugins: [
     new HTMLWebpackPlugin({
@@ -14,6 +27,11 @@ module.exports = {
       },
       hash: true,
       template: './src/index.ejs' // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new ExtractTextWebpackPlugin({
+      filename: 'app.css',
+      allChunks: true,
+      disable: false
     })
   ]
 };
