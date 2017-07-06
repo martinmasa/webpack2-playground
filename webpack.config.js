51a5dc4d3b755846343efacf920e1694cab2adac
Martin M <mxa.code@gmail.com>
2017-07-06 23:42:11 +0100

    Add HTML Webpack Plugin: basic config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
-  }
+    path: `${__dirname}/dist`,
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Webpack demo',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.ejs' // Load a custom template (ejs by default see the FAQ for details)
+    })
+  ]
 };
